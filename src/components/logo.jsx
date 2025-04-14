357eea83e783b47ad1c804742a8045d8124993d2
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-04-14 16:52:20 +0530

    Update logo reference in index.html and Logo component; add new logo file

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
+
 function Logo({}){
     return(
-        <img src="./ChatGPT Image Apr 14, 2025, 03_42_44 PM.png" alt=""
+        <img src="./Logo.png" alt=""
          className="w-20 h-16 object-cover rounded"
         />
     )
